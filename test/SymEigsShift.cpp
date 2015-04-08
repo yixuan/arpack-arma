6b2c652ef3f4b12a95b655d248dc8bce5d3a85be
Yixuan Qiu <yixuan.qiu@cos.name>
2015-04-08 00:22:06 -0400

    update testing examples

--- test/SymEigsShift.cpp
+++ test/SymEigsShift.cpp
@@ -8,7 +8,7 @@ typedef arma::mat Matrix;
 typedef arma::vec Vector;
 
 template <int SelectionRule>
-void test(const Matrix &A, int k, int m)
+void test(const Matrix &A, int k, int m, double sigma)
 {
     Matrix mat;
     if(SelectionRule == BOTH_ENDS)
@@ -22,19 +22,27 @@ void test(const Matrix &A, int k, int m)
     all_eval.t().print("all eigenvalues =");
 
     MatOpDenseSym<double> op(mat);
-    SymEigsSolver<double, SelectionRule> eigs(&op, k, m);
+    SymEigsShiftSolver<double, SelectionRule> eigs(&op, k, m, sigma);
     eigs.init();
-    int nconv = eigs.compute();
+    int nconv = 0;
+    try {
+        nconv = eigs.compute();
+    } catch(std::exception &e) {
+        std::cout << "ERROR: " << e.what() << std::endl;
+    }
+
     int niter, nops;
     eigs.info(niter, nops);
 
-    Vector evals = eigs.eigenvalues();
-    Matrix evecs = eigs.eigenvectors();
-
-    evals.print("computed eigenvalues D =");
-    //evecs.print("computed eigenvectors U =");
-    Matrix err = mat * evecs - evecs * arma::diagmat(evals);
-    std::cout << "||AU - UD||_inf = " << arma::abs(err).max() << std::endl;
+    if(nconv > 0)
+    {
+        Vector evals = eigs.eigenvalues();
+        Matrix evecs = eigs.eigenvectors();
+        evals.print("computed eigenvalues D =");
+        //evecs.print("computed eigenvectors U =");
+        Matrix err = mat * evecs - evecs * arma::diagmat(evals);
+        std::cout << "||AU - UD||_inf = " << arma::abs(err).max() << std::endl;
+    }
     std::cout << "nconv = " << nconv << std::endl;
     std::cout << "niter = " << niter << std::endl;
     std::cout << "nops = " << nops << std::endl;
@@ -47,21 +55,22 @@ int main()
 
     int k = 3;
     int m = 6;
+    double sigma = 1.0;
 
     std::cout << "===== Largest Magnitude =====\n";
-    test<LARGEST_MAGN>(A, k, m);
+    test<LARGEST_MAGN>(A, k, m, sigma);
 
     std::cout << "\n===== Largest Value =====\n";
-    test<LARGEST_ALGE>(A, k, m);
+    test<LARGEST_ALGE>(A, k, m, sigma);
 
     std::cout << "\n===== Smallest Magnitude =====\n";
-    test<SMALLEST_MAGN>(A, k, m);
+    test<SMALLEST_MAGN>(A, k, m, sigma);
 
     std::cout << "\n===== Smallest Value =====\n";
-    test<SMALLEST_ALGE>(A, k, m);
+    test<SMALLEST_ALGE>(A, k, m, sigma);
 
     std::cout << "\n===== Both Ends =====\n";
-    test<BOTH_ENDS>(A, k, m);
+    test<BOTH_ENDS>(A, k, m, sigma);
 
     return 0;
 }
