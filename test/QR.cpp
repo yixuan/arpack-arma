cb8f52f8f3966998e749bd3ab4b2256c1305964c
Yixuan Qiu <yixuan.qiu@cos.name>
2015-05-26 20:10:39 -0400

    use Catch to write unit test

--- test/QR.cpp
+++ test/QR.cpp
@@ -1,10 +1,13 @@
 // Test ../include/UpperHessenbergQR.h
 #include <UpperHessenbergQR.h>
 
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
 using arma::mat;
 using arma::vec;
 
-void QR_UpperHessenberg()
+TEST_CASE("QR of upper Hessenberg matrix", "[qr]")
 {
     arma::arma_rng::set_seed(123);
     int n = 100;
@@ -21,53 +24,62 @@ void QR_UpperHessenberg()
 
     // Test orthogonality
     mat QtQ = Q.t() * Q;
-    std::cout << "||Q'Q - I||_inf = " << arma::abs(QtQ - I).max() << std::endl;
+    INFO( "||Q'Q - I||_inf = " << arma::abs(QtQ - I).max() );
+    REQUIRE( arma::abs(QtQ - I).max() == Approx(0.0) );
 
     mat QQt = Q * Q.t();
-    std::cout << "||QQ' - I||_inf = " << arma::abs(QQt - I).max() << std::endl;
+    INFO( "||QQ' - I||_inf = " << arma::abs(QQt - I).max() );
+    REQUIRE( arma::abs(QQt - I).max() == Approx(0.0) );
 
     // Calculate R = Q'H
     mat R = decomp.matrix_R();
     mat Rlower = arma::trimatl(R);
     Rlower.diag().zeros();
-    std::cout << "whether R is upper triangular, error = "
-              << arma::abs(Rlower).max() << std::endl;
+    INFO( "Whether R is upper triangular, error = " << arma::abs(Rlower).max() );
+    REQUIRE( arma::abs(Rlower).max() == Approx(0.0) );
 
     // Compare H and QR
-    std::cout << "||H - QR||_inf = " << arma::abs(H - Q * R).max() << std::endl;
+    INFO( "||H - QR||_inf = " << arma::abs(H - Q * R).max() );
+    REQUIRE( arma::abs(H - Q * R).max() == Approx(0.0) );
 
     // Test "apply" functions
     mat Y(n, n, arma::fill::randn);
 
     mat QY = Y;
     decomp.apply_QY(QY);
-    std::cout << "max error of QY = " << arma::abs(QY - Q * Y).max() << std::endl;
+    INFO( "max error of QY = " << arma::abs(QY - Q * Y).max() );
+    REQUIRE( arma::abs(QY - Q * Y).max() == Approx(0.0) );
 
     mat YQ = Y;
     decomp.apply_YQ(YQ);
-    std::cout << "max error of YQ = " << arma::abs(YQ - Y * Q).max() << std::endl;
+    INFO( "max error of YQ = " << arma::abs(YQ - Y * Q).max() );
+    REQUIRE( arma::abs(YQ - Y * Q).max() == Approx(0.0) );
 
     mat QtY = Y;
     decomp.apply_QtY(QtY);
-    std::cout << "max error of Q'Y = " << arma::abs(QtY - Q.t() * Y).max() << std::endl;
+    INFO( "max error of Q'Y = " << arma::abs(QtY - Q.t() * Y).max() );
+    REQUIRE( arma::abs(QtY - Q.t() * Y).max() == Approx(0.0) );
 
     mat YQt = Y;
     decomp.apply_YQt(YQt);
-    std::cout << "max error of YQ' = " << arma::abs(YQt - Y * Q.t()).max() << std::endl;
+    INFO( "max error of YQ' = " << arma::abs(YQt - Y * Q.t()).max() );
+    REQUIRE( arma::abs(YQt - Y * Q.t()).max() == Approx(0.0) );
 
     // Test "apply" functions for vectors
     vec y(n, arma::fill::randn);
 
     vec Qy = y;
     decomp.apply_QY(Qy);
-    std::cout << "max error of Qy = " << arma::abs(Qy - Q * y).max() << std::endl;
+    INFO( "max error of Qy = " << arma::abs(Qy - Q * y).max() );
+    REQUIRE( arma::abs(Qy - Q * y).max() == Approx(0.0) );
 
     vec Qty = y;
     decomp.apply_QtY(Qty);
-    std::cout << "max error of Q'y = " << arma::abs(Qty - Q.t() * y).max() << std::endl;
+    INFO( "max error of Q'y = " << arma::abs(Qty - Q.t() * y).max() );
+    REQUIRE( arma::abs(Qty - Q.t() * y).max() == Approx(0.0) );
 }
 
-void QR_Tridiagonal()
+TEST_CASE("QR of Tridiagonal matrix", "[qr]")
 {
     arma::arma_rng::set_seed(123);
     int n = 100;
@@ -86,65 +98,63 @@ void QR_Tridiagonal()
 
     // Test orthogonality
     mat QtQ = Q.t() * Q;
-    QtQ.diag() -= 1.0;
-    std::cout << "||Q'Q - I||_inf = " << arma::abs(QtQ).max() << std::endl;
+    INFO( "||Q'Q - I||_inf = " << arma::abs(QtQ - I).max() );
+    REQUIRE( arma::abs(QtQ - I).max() == Approx(0.0) );
 
     mat QQt = Q * Q.t();
-    QQt.diag() -= 1.0;
-    std::cout << "||QQ' - I||_inf = " << arma::abs(QQt).max() << std::endl;
+    INFO( "||QQ' - I||_inf = " << arma::abs(QQt - I).max() );
+    REQUIRE( arma::abs(QQt - I).max() == Approx(0.0) );
 
-    // Whether R is upper triangular
+    // Calculate R = Q'H
     mat R = decomp.matrix_R();
     mat Rlower = arma::trimatl(R);
     Rlower.diag().zeros();
-    std::cout << "whether R is upper triangular, error = "
-              << arma::abs(Rlower).max() << std::endl;
+    INFO( "Whether R is upper triangular, error = " << arma::abs(Rlower).max() );
+    REQUIRE( arma::abs(Rlower).max() == Approx(0.0) );
 
     // Compare H and QR
-    std::cout << "||H - QR||_inf = " << arma::abs(H - Q * R).max() << std::endl;
+    INFO( "||H - QR||_inf = " << arma::abs(H - Q * R).max() );
+    REQUIRE( arma::abs(H - Q * R).max() == Approx(0.0) );
 
     // Test RQ
     mat rq = R;
     decomp.apply_YQ(rq);
-    std::cout << "max error of RQ = " << arma::abs(decomp.matrix_RQ() - rq).max() << std::endl;
+    INFO( "max error of RQ = " << arma::abs(decomp.matrix_RQ() - rq).max() );
+    REQUIRE( arma::abs(decomp.matrix_RQ() - rq).max() == Approx(0.0) );
 
     // Test "apply" functions
     mat Y(n, n, arma::fill::randn);
 
     mat QY = Y;
     decomp.apply_QY(QY);
-    std::cout << "max error of QY = " << arma::abs(QY - Q * Y).max() << std::endl;
+    INFO( "max error of QY = " << arma::abs(QY - Q * Y).max() );
+    REQUIRE( arma::abs(QY - Q * Y).max() == Approx(0.0) );
 
     mat YQ = Y;
     decomp.apply_YQ(YQ);
-    std::cout << "max error of YQ = " << arma::abs(YQ - Y * Q).max() << std::endl;
+    INFO( "max error of YQ = " << arma::abs(YQ - Y * Q).max() );
+    REQUIRE( arma::abs(YQ - Y * Q).max() == Approx(0.0) );
 
     mat QtY = Y;
     decomp.apply_QtY(QtY);
-    std::cout << "max error of Q'Y = " << arma::abs(QtY - Q.t() * Y).max() << std::endl;
+    INFO( "max error of Q'Y = " << arma::abs(QtY - Q.t() * Y).max() );
+    REQUIRE( arma::abs(QtY - Q.t() * Y).max() == Approx(0.0) );
 
     mat YQt = Y;
     decomp.apply_YQt(YQt);
-    std::cout << "max error of YQ' = " << arma::abs(YQt - Y * Q.t()).max() << std::endl;
+    INFO( "max error of YQ' = " << arma::abs(YQt - Y * Q.t()).max() );
+    REQUIRE( arma::abs(YQt - Y * Q.t()).max() == Approx(0.0) );
 
     // Test "apply" functions for vectors
     vec y(n, arma::fill::randn);
 
     vec Qy = y;
     decomp.apply_QY(Qy);
-    std::cout << "max error of Qy = " << arma::abs(Qy - Q * y).max() << std::endl;
+    INFO( "max error of Qy = " << arma::abs(Qy - Q * y).max() );
+    REQUIRE( arma::abs(Qy - Q * y).max() == Approx(0.0) );
 
     vec Qty = y;
     decomp.apply_QtY(Qty);
-    std::cout << "max error of Q'y = " << arma::abs(Qty - Q.t() * y).max() << std::endl;
-}
-
-int main()
-{
-    std::cout << "========== Test of upper Hessenberg matrix ==========\n";
-    QR_UpperHessenberg();
-
-    std::cout << "\n========== Test of Tridiagonal matrix ==========\n";
-    QR_Tridiagonal();
-
+    INFO( "max error of Q'y = " << arma::abs(Qty - Q.t() * y).max() );
+    REQUIRE( arma::abs(Qty - Q.t() * y).max() == Approx(0.0) );
 }
