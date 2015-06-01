55a6da723cea93fe8b1c2d98bfc2bc9b58764dd8
Yixuan Qiu <yixuan.qiu@cos.name>
2015-05-31 21:00:33 -0400

    simplify class structure

--- include/MatOp.h
+++ include/MatOp.h
@@ -4,36 +4,23 @@
 template <typename Scalar>
 class MatOp
 {
-private:
-    // Dimension of the matrix
-    // m rows and n columns
-    // In eigenvalue problems, they are assumed to be equal.
-    const int m;
-    const int n;
-
 public:
-    // Constructor
-    MatOp(int m_, int n_) :
-        m(m_), n(n_)
-    {}
     // Destructor
     virtual ~MatOp() {}
 
     // y_out = A * x_in
     virtual void prod(Scalar *x_in, Scalar *y_out) = 0;
 
-    int rows() { return m; }
-    int cols() { return n; }
+    // Dimension of the matrix
+    // In eigenvalue problems, they should be equal.
+    virtual int rows() = 0;
+    virtual int cols() = 0;
 };
 
 template <typename Scalar>
-class MatOpWithTransProd: public virtual MatOp<Scalar>
+class MatOpWithTransProd: virtual public MatOp<Scalar>
 {
 public:
-    // Constructor
-    MatOpWithTransProd(int m_, int n_) :
-        MatOp<Scalar>(m_, n_)
-    {}
     // Destructor
     virtual ~MatOpWithTransProd() {}
 
@@ -42,13 +29,9 @@ public:
 };
 
 template <typename Scalar>
-class MatOpWithRealShiftSolve: public virtual MatOp<Scalar>
+class MatOpWithRealShiftSolve: virtual public MatOp<Scalar>
 {
 public:
-    // Constructor
-    MatOpWithRealShiftSolve(int m_, int n_) :
-        MatOp<Scalar>(m_, n_)
-    {}
     // Destructor
     virtual ~MatOpWithRealShiftSolve() {}
 
@@ -62,11 +45,6 @@ template <typename Scalar>
 class MatOpWithComplexShiftSolve: public MatOpWithRealShiftSolve<Scalar>
 {
 public:
-    // Constructor
-    MatOpWithComplexShiftSolve(int m_, int n_) :
-        MatOp<Scalar>(m_, n_),
-        MatOpWithRealShiftSolve<Scalar>(m_, n_)
-    {}
     // Destructor
     virtual ~MatOpWithComplexShiftSolve() {}
 
