d0600e34a774e1caf40568b09540ecea906062e3
Yixuan Qiu <yixuan.qiu@cos.name>
2015-07-06 08:56:13 -0400

    wrapper for *hsein

--- include/LinAlg/LapackWrapperExtra.h
+++ include/LinAlg/LapackWrapperExtra.h
@@ -27,6 +27,9 @@ namespace arma
         #define arma_strevc strevc
         #define arma_dtrevc dtrevc
 
+        #define arma_shsein shsein
+        #define arma_dhsein dhsein
+
     #else
 
         #define arma_ssytrs SSYTRS
@@ -44,6 +47,9 @@ namespace arma
 
         #define arma_strevc STREVC
         #define arma_dtrevc DTREVC
+
+        #define arma_shsein SHSEIN
+        #define arma_dhsein DHSEIN
 
     #endif
 
@@ -66,6 +72,9 @@ namespace arma
 
         void arma_fortran(arma_strevc)(char* side, char* howmny, blas_int* select, blas_int* n, float*  t, blas_int* ldt, float*  vl, blas_int* ldvl, float*  vr, blas_int* ldvr, blas_int* mm, blas_int* m, float*  work, blas_int* info);
         void arma_fortran(arma_dtrevc)(char* side, char* howmny, blas_int* select, blas_int* n, double* t, blas_int* ldt, double* vl, blas_int* ldvl, double* vr, blas_int* ldvr, blas_int* mm, blas_int* m, double* work, blas_int* info);
+
+        void arma_fortran(arma_shsein)(char* side, char* eigsrc, char* initv, blas_int* select, blas_int* n, float*  h, blas_int* ldh, float*  wr, float*  wi, float*  vl, blas_int* ldvl, float*  vr, blas_int* ldvr, blas_int* mm, blas_int* m, float*  work, blas_int* ifaill, blas_int* ifailr, blas_int* info);
+        void arma_fortran(arma_dhsein)(char* side, char* eigsrc, char* initv, blas_int* select, blas_int* n, double* h, blas_int* ldh, double* wr, double* wi, double* vl, blas_int* ldvl, double* vr, blas_int* ldvr, blas_int* mm, blas_int* m, double* work, blas_int* ifaill, blas_int* ifailr, blas_int* info);
     }
 
 
@@ -169,6 +178,25 @@ namespace arma
             {
                 typedef double T;
                 arma_fortran(arma_dtrevc)(side, howmny, select, n, (T*)t, ldt, (T*)vl, ldvl, (T*)vr, ldvr, mm, m, (T*)work, info);
+            }
+        }
+
+        template<typename eT>
+        inline
+        void
+        hsein(char* side, char* eigsrc, char* initv, blas_int* select, blas_int* n, eT* h, blas_int* ldh, eT* wr, eT* wi, eT* vl, blas_int* ldvl, eT* vr, blas_int* ldvr, blas_int* mm, blas_int* m, eT* work, blas_int* ifaill, blas_int* ifailr, blas_int* info)
+        {
+            arma_type_check(( is_supported_blas_type<eT>::value == false ));
+            if(is_float<eT>::value == true)
+            {
+                typedef float T;
+                arma_fortran(arma_shsein)(side, eigsrc, initv, select, n, (T*)h, ldh, (T*)wr, (T*)wi, (T*)vl, ldvl, (T*)vr, ldvr, mm, m, (T*)work, ifaill, ifailr, info);
+            }
+            else
+            if(is_double<eT>::value == true)
+            {
+                typedef double T;
+                arma_fortran(arma_dhsein)(side, eigsrc, initv, select, n, (T*)h, ldh, (T*)wr, (T*)wi, (T*)vl, ldvl, (T*)vr, ldvr, mm, m, (T*)work, ifaill, ifailr, info);
             }
         }
     }
