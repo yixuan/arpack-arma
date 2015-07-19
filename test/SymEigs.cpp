0031e087c7a663dbbd4827f6efaa78047610dd35
Yixuan Qiu <yixuan.qiu@cos.name>
2015-07-19 09:03:00 -0400

    simplify matrix creation

--- test/SymEigs.cpp
+++ test/SymEigs.cpp
@@ -10,16 +10,8 @@ typedef arma::mat Matrix;
 typedef arma::vec Vector;
 
 template <int SelectionRule>
-void run_test(const Matrix &A, int k, int m)
+void run_test(Matrix &mat, int k, int m)
 {
-    Matrix mat;
-    if(SelectionRule == BOTH_ENDS)
-    {
-        mat = A.t() + A;
-    } else {
-        mat = A.t() * A;
-    }
-
     // Vector all_eval = arma::eig_sym(mat);
     // all_eval.t().print("all eigenvalues =");
 
@@ -46,27 +38,27 @@ void run_test(const Matrix &A, int k, int m)
     REQUIRE( arma::abs(err).max() == Approx(0.0) );
 }
 
-void run_test_sets(const Matrix &A, int k, int m)
+void run_test_sets(Matrix &mat, int k, int m)
 {
     SECTION( "Largest Magnitude" )
     {
-        run_test<LARGEST_MAGN>(A, k, m);
+        run_test<LARGEST_MAGN>(mat, k, m);
     }
     SECTION( "Largest Value" )
     {
-        run_test<LARGEST_ALGE>(A, k, m);
+        run_test<LARGEST_ALGE>(mat, k, m);
     }
     SECTION( "Smallest Magnitude" )
     {
-        run_test<SMALLEST_MAGN>(A, k, m);
+        run_test<SMALLEST_MAGN>(mat, k, m);
     }
     SECTION( "Smallest Value" )
     {
-        run_test<SMALLEST_ALGE>(A, k, m);
+        run_test<SMALLEST_ALGE>(mat, k, m);
     }
     SECTION( "Both Ends" )
     {
-        run_test<BOTH_ENDS>(A, k, m);
+        run_test<BOTH_ENDS>(mat, k, m);
     }
 }
 
@@ -75,9 +67,10 @@ TEST_CASE("Eigensolver of symmetric real matrix [10x10]", "[eigs_sym]")
     arma::arma_rng::set_seed(123);
 
     Matrix A = arma::randu(10, 10);
+    Matrix mat = A + A.t();
     int k = 3;
     int m = 6;
-    run_test_sets(A, k, m);
+    run_test_sets(mat, k, m);
 }
 
 TEST_CASE("Eigensolver of symmetric real matrix [100x100]", "[eigs_sym]")
@@ -85,9 +78,10 @@ TEST_CASE("Eigensolver of symmetric real matrix [100x100]", "[eigs_sym]")
     arma::arma_rng::set_seed(123);
 
     Matrix A = arma::randu(100, 100);
+    Matrix mat = A + A.t();
     int k = 10;
     int m = 20;
-    run_test_sets(A, k, m);
+    run_test_sets(mat, k, m);
 }
 
 TEST_CASE("Eigensolver of symmetric real matrix [1000x1000]", "[eigs_sym]")
@@ -95,7 +89,8 @@ TEST_CASE("Eigensolver of symmetric real matrix [1000x1000]", "[eigs_sym]")
     arma::arma_rng::set_seed(123);
 
     Matrix A = arma::randu(1000, 1000);
+    Matrix mat = A + A.t();
     int k = 20;
     int m = 50;
-    run_test_sets(A, k, m);
+    run_test_sets(mat, k, m);
 }
