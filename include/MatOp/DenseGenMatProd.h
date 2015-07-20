3f252f6300b2d1f9204bdcc80bace7064cd2a5b1
Yixuan Qiu <yixuan.qiu@cos.name>
2015-07-20 15:24:14 -0400

    document MatOp classes

--- include/MatOp/DenseGenMatProd.h
+++ include/MatOp/DenseGenMatProd.h
@@ -3,6 +3,18 @@
 
 #include <armadillo>
 
+///
+/// \defgroup MatOp Matrix Operations
+///
+
+///
+/// \ingroup MatOp
+///
+/// This class defines the matrix-vector multiplication operation on a
+/// general real matrix \f$A\f$, i.e., calculating \f$y=Ax\f$ for any vector
+/// \f$x\f$. It is mainly used in the GenEigsSolver and
+/// SymEigsSolver eigen solvers.
+///
 template <typename Scalar>
 class DenseGenMatProd
 {
@@ -13,13 +25,31 @@ private:
     const Matrix mat;
 
 public:
+    ///
+    /// Constructor to create the matrix operation object.
+    ///
+    /// \param mat_ An **Armadillo** matrix object, whose type can be `arma::mat`
+    ///             or `arma::fmat`, depending on the template parameter `Scalar` defined.
+    ///
     DenseGenMatProd(Matrix &mat_) :
         mat(mat_.memptr(), mat_.n_rows, mat_.n_cols, false)
     {}
 
+    ///
+    /// Return the number of rows of the underlying matrix.
+    ///
     int rows() { return mat.n_rows; }
+    ///
+    /// Return the number of columns of the underlying matrix.
+    ///
     int cols() { return mat.n_cols; }
 
+    ///
+    /// Perform the matrix-vector multiplication operation \f$y=Ax\f$.
+    ///
+    /// \param x_in  Pointer to the \f$x\f$ vector.
+    /// \param y_out Pointer to the \f$y\f$ vector.
+    ///
     // y_out = A * x_in
     void perform_op(Scalar *x_in, Scalar *y_out)
     {
--- include/MatOp/DenseSymShiftSolve.h
+++ include/MatOp/DenseSymShiftSolve.h
@@ -5,6 +5,13 @@
 #include <stdexcept>
 #include "../LinAlg/SymmetricLDL.h"
 
+///
+/// \ingroup MatOp
+///
+/// This class defines the shift-solve operation on a real symmetric matrix \f$A\f$,
+/// i.e., calculating \f$y=(A-\sigma I)^{-1}x\f$ for any real \f$\sigma\f$ and
+/// vector \f$x\f$. It is mainly used in the SymEigsShiftSolver eigen solver.
+///
 template <typename Scalar>
 class DenseSymShiftSolve
 {
@@ -16,6 +23,12 @@ private:
     const int dim_n;
     SymmetricLDL<Scalar> solver;
 public:
+    ///
+    /// Constructor to create the matrix operation object.
+    ///
+    /// \param mat_ An **Armadillo** matrix object, whose type can be `arma::mat`
+    ///             or `arma::fmat`, depending on the template parameter `Scalar` defined.
+    ///
     DenseSymShiftSolve(Matrix &mat_) :
         mat(mat_.memptr(), mat_.n_rows, mat_.n_cols, false),
         dim_n(mat_.n_rows)
@@ -24,15 +37,29 @@ public:
             throw std::invalid_argument("DenseSymShiftSolve: matrix must be square");
     }
 
+    ///
+    /// Return the number of rows of the underlying matrix.
+    ///
     int rows() { return dim_n; }
+    ///
+    /// Return the number of columns of the underlying matrix.
+    ///
     int cols() { return dim_n; }
 
-    // setting real sigma
+    ///
+    /// Set the real shift \f$\sigma\f$.
+    ///
     void set_shift(Scalar sigma)
     {
         solver.compute(mat - sigma * arma::eye<Matrix>(dim_n, dim_n));
     }
 
+    ///
+    /// Perform the shift-solve operation \f$y=(A-\sigma I)^{-1}x\f$.
+    ///
+    /// \param x_in  Pointer to the \f$x\f$ vector.
+    /// \param y_out Pointer to the \f$y\f$ vector.
+    ///
     // y_out = inv(A - sigma * I) * x_in
     void perform_op(Scalar *x_in, Scalar *y_out)
     {
