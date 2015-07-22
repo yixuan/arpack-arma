e005dfc079149f36695ffb6806e8dffe6da70d2a
Yixuan Qiu <yixuan.qiu@cos.name>
2015-07-21 22:32:26 -0400

    testing of TridiagEigen

--- test/Eigen.cpp
+++ test/Eigen.cpp
@@ -1,5 +1,6 @@
-// Test ../include/LinAlg/UpperHessenbergEigen.h
+// Test ../include/LinAlg/UpperHessenbergEigen.h and ../include/LinAlg/TridiagEigen.h
 #include <LinAlg/UpperHessenbergEigen.h>
+#include <LinAlg/TridiagEigen.h>
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
@@ -47,5 +48,48 @@ TEST_CASE("Eigen decomposition of upper Hessenberg matrix", "[Eigen]")
     }
     t2 = clock();
     std::cout << "elapsed time for arma::eig_gen: "
+              << double(t2 - t1) / CLOCKS_PER_SEC << " secs\n";
+}
+
+TEST_CASE("Eigen decomposition of symmetric tridiagonal matrix", "[Eigen]")
+{
+    arma::arma_rng::set_seed(123);
+    int n = 100;
+    mat m(n, n, arma::fill::randn);
+    mat H(n, n, arma::fill::zeros);
+    H.diag() = m.diag();
+    H.diag(-1) = m.diag(-1);
+    H.diag(1) = m.diag(-1);
+
+    TridiagEigen<double> decomp(H);
+    vec evals = decomp.eigenvalues();
+    mat evecs = decomp.eigenvectors();
+
+    mat err = H * evecs - evecs * arma::diagmat(evals);
+
+    INFO( "||HU - UD||_inf = " << arma::abs(err).max() );
+    REQUIRE( arma::abs(err).max() == Approx(0.0) );
+
+    clock_t t1, t2;
+    t1 = clock();
+    for(int i = 0; i < 100; i++)
+    {
+        TridiagEigen<double> decomp(H);
+        vec evals = decomp.eigenvalues();
+        mat evecs = decomp.eigenvectors();
+    }
+    t2 = clock();
+    std::cout << "elapsed time for TridiagEigen: "
+              << double(t2 - t1) / CLOCKS_PER_SEC << " secs\n";
+
+    t1 = clock();
+    for(int i = 0; i < 100; i++)
+    {
+        vec evals(n);
+        mat evecs(n, n);
+        arma::eig_sym(evals, evecs, H);
+    }
+    t2 = clock();
+    std::cout << "elapsed time for arma::eig_sym: "
               << double(t2 - t1) / CLOCKS_PER_SEC << " secs\n";
 }
