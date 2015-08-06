356fe3efdb064e0c4e518c1ded795500022b8d1f
Yixuan Qiu <yixuan.qiu@cos.name>
2015-08-06 18:48:19 -0400

    formatting

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -23,12 +23,12 @@ void print_header(std::string title)
     std::cout << std::string((width - title.length()) / 2, ' ') << title << std::endl;
     std::cout << std::string(width, '-') << std::endl;
 
-    std::cout << std::left << std::setw(11) << std::setfill(sep) << "mat_size";
-    std::cout << std::left << std::setw(10) << std::setfill(sep) << "dataset";
-    std::cout << std::left << std::setw(16) << std::setfill(sep) << "F77_time (ms)";
-    std::cout << std::left << std::setw(15) << std::setfill(sep) << "F77_err (ms)";
-    std::cout << std::left << std::setw(16) << std::setfill(sep) << "C++_time (ms)";
-    std::cout << std::left << std::setw(12) << std::setfill(sep) << "C++_err (ms)";
+    std::cout << std::left << std::setw(12) << std::setfill(sep) << "mat_size";
+    std::cout << std::left << std::setw(11) << std::setfill(sep) << "dataset";
+    std::cout << std::left << std::setw(15) << std::setfill(sep) << "F77_time/ms";
+    std::cout << std::left << std::setw(13) << std::setfill(sep) << "F77_error";
+    std::cout << std::left << std::setw(15) << std::setfill(sep) << "C++_time/ms";
+    std::cout << std::left << std::setw(13) << std::setfill(sep) << "C++_error";
     std::cout << std::endl;
 
     std::cout << std::string(width, '-') << std::endl;
@@ -38,12 +38,12 @@ void print_row(int n, int dataset, double time_f77, double err_f77, double time_
 {
     const char sep = ' ';
 
-    std::cout << std::left << std::setw(11) << std::setfill(sep) << n;
-    std::cout << std::left << std::setw(10) << std::setfill(sep) << dataset;
-    std::cout << std::left << std::setw(16) << std::setfill(sep) << time_f77;
-    std::cout << std::left << std::setw(15) << std::setfill(sep) << err_f77;
-    std::cout << std::left << std::setw(16) << std::setfill(sep) << time_cpp;
-    std::cout << std::left << std::setw(12) << std::setfill(sep) << err_cpp;
+    std::cout << std::left << std::setw(12) << std::setfill(sep) << n;
+    std::cout << std::left << std::setw(11) << std::setfill(sep) << dataset;
+    std::cout << std::left << std::setw(15) << std::setfill(sep) << time_f77;
+    std::cout << std::left << std::setw(13) << std::setfill(sep) << err_f77;
+    std::cout << std::left << std::setw(15) << std::setfill(sep) << time_cpp;
+    std::cout << std::left << std::setw(13) << std::setfill(sep) << err_cpp;
     std::cout << std::endl;
 }
 
@@ -160,7 +160,7 @@ int main()
     run_eigs_gen(n_experiment, n_replicate, 1000, 10, 30);
     print_footer();
 
-    print_header("sparse_eigs_sym");
+    /*print_header("sparse_eigs_sym");
     run_sparse_eigs_sym(n_experiment, n_replicate, 100, 10, 20);
     run_sparse_eigs_sym(n_experiment, n_replicate, 1000, 10, 30);
     print_footer();
@@ -168,7 +168,7 @@ int main()
     print_header("sparse_eigs_gen");
     run_sparse_eigs_gen(n_experiment, n_replicate, 100, 10, 20);
     run_sparse_eigs_gen(n_experiment, n_replicate, 1000, 10, 30);
-    print_footer();
+    print_footer();*/
 
     return 0;
 }
