81b8faa3984ba2746a5b8b59fd3bcbcbac3a5478
Yixuan Qiu <yixuan.qiu@cos.name>
2016-02-29 17:20:35 -0500

    documentation

--- refactoring/eigs_linalg_hessenqr_bones.hpp
+++ refactoring/eigs_linalg_hessenqr_bones.hpp
@@ -5,20 +5,28 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-// Perform the QR decomposition of an upper Hessenberg matrix.
+//! Perform the QR decomposition of an upper Hessenberg matrix.
 template<typename eT>
 class UpperHessenbergQR
   {
   public:
 
+  //! Default constructor. Computation can
+  //! be performed later by calling the compute() method.
   inline UpperHessenbergQR();
 
+  //! Constructor to create an object that performs and stores the
+  //! QR decomposition of an upper Hessenberg matrix `mat`.
   inline UpperHessenbergQR(const Mat<eT>& mat);
 
+  //! Conduct the QR factorization of an upper Hessenberg matrix.
   virtual void compute(const Mat<eT>& mat);
 
+  //! Return the \f$RQ\f$ matrix, the multiplication of \f$R\f$ and \f$Q\f$,
+  //! which is an upper Hessenberg matrix.
   virtual Mat<eT> matrix_RQ();
 
+  //! Apply the \f$Q\f$ matrix to another matrix \f$Y\f$.
   inline void apply_YQ(Mat<eT>& Y);
 
 
@@ -43,11 +51,18 @@ class TridiagQR : public UpperHessenbergQR<eT>
   {
   public:
 
+  //! Default constructor. Computation can
+  //! be performed later by calling the compute() method.
   inline TridiagQR();
 
+  //! Constructor to create an object that performs and stores the
+  //! QR decomposition of a tridiagonal matrix `mat`.
   inline TridiagQR(const Mat<eT>& mat);
 
+  //! Conduct the QR factorization of a tridiagonal matrix.
   inline void compute(const Mat<eT>& mat);
 
+  //! Return the \f$RQ\f$ matrix, the multiplication of \f$R\f$ and \f$Q\f$,
+  //! which is a tridiagonal matrix.
   inline Mat<eT> matrix_RQ();
   };
