b1bd1f8539b3b531f75ef00c9eb1d7b810a650b1
Yixuan Qiu <yixuan.qiu@cos.name>
2016-02-29 22:11:25 -0500

    add test code

--- refactoring/test/Eigen.cpp
+++ refactoring/test/Eigen.cpp
@@ -2,6 +2,8 @@
 namespace arma {
     #include "../eigs_linalg_hesseneigen_bones.hpp"
     #include "../eigs_linalg_hesseneigen_meat.hpp"
+    #include "../eigs_linalg_tridiageigen_bones.hpp"
+    #include "../eigs_linalg_tridiageigen_meat.hpp"
 }
 
 #define CATCH_CONFIG_MAIN
@@ -12,6 +14,7 @@ using arma::vec;
 using arma::cx_vec;
 using arma::cx_mat;
 using arma::UpperHessenbergEigen;
+using arma::TridiagEigen;
 
 TEST_CASE("Eigen decomposition of upper Hessenberg matrix", "[Eigen]")
 {
@@ -51,5 +54,48 @@ TEST_CASE("Eigen decomposition of upper Hessenberg matrix", "[Eigen]")
     }
     t2 = clock();
     std::cout << "elapsed time for arma::eig_gen: "
+              << double(t2 - t1) / CLOCKS_PER_SEC << " secs\n";
+}
+
+TEST_CASE("Eigen decomposition of symmetric tridiagonal matrix", "[Eigen]")
+{
+    arma::arma_rng::set_seed(123);
+    int n = 100;
+    mat m(n, n, arma::fill::randn);
+    mat H(n, n, arma::fill::zeros);
+    H.diag() = m.diag();
+    H.diag(-1) = m.diag(-1);
+    H.diag(1) = m.diag(-1);
+
+    TridiagEigen<double> decomp(H);
+    vec evals = decomp.eigenvalues();
+    mat evecs = decomp.eigenvectors();
+
+    mat err = H * evecs - evecs * arma::diagmat(evals);
+
+    INFO( "||HU - UD||_inf = " << arma::abs(err).max() );
+    REQUIRE( arma::abs(err).max() == Approx(0.0) );
+
+    clock_t t1, t2;
+    t1 = clock();
+    for(int i = 0; i < 100; i++)
+    {
+        TridiagEigen<double> decomp(H);
+        vec evals = decomp.eigenvalues();
+        mat evecs = decomp.eigenvectors();
+    }
+    t2 = clock();
+    std::cout << "elapsed time for TridiagEigen: "
+              << double(t2 - t1) / CLOCKS_PER_SEC << " secs\n";
+
+    t1 = clock();
+    for(int i = 0; i < 100; i++)
+    {
+        vec evals(n);
+        mat evecs(n, n);
+        arma::eig_sym(evals, evecs, H);
+    }
+    t2 = clock();
+    std::cout << "elapsed time for arma::eig_sym: "
               << double(t2 - t1) / CLOCKS_PER_SEC << " secs\n";
 }
