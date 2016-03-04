34d37de697b43f677928997a0561d2f1b1c2355c
Yixuan Qiu <yixuan.qiu@cos.name>
2016-03-04 16:24:22 -0500

    testing code for eigs_gen()

--- refactoring/test/ArmaGenEigs.cpp
+++ refactoring/test/ArmaGenEigs.cpp
@@ -8,14 +8,15 @@ typedef arma::uword uword;
 
 void run_test(arma::sp_mat& mat, uword k, const char* form)
 {
-    arma::vec eigval;
-    arma::mat eigvec;
+    arma::cx_vec eigval;
+    arma::cx_mat eigvec;
 
-    bool status = arma::eigs_sym(eigval, eigvec, mat, k, form);
+    bool status = arma::eigs_gen(eigval, eigvec, mat, k, form);
 
     REQUIRE( status == true );
 
-    arma::mat err = mat * eigvec - eigvec * arma::diagmat(eigval);
+    arma::mat dmat(mat);
+    arma::cx_mat err = dmat * eigvec - eigvec * arma::diagmat(eigval);
 
     INFO( "||AU - UD||_inf = " << arma::abs(err).max() );
     REQUIRE( arma::abs(err).max() == Approx(0.0) );
@@ -31,45 +32,50 @@ void run_test_sets(arma::sp_mat& mat, uword k)
     {
         run_test(mat, k, "sm");
     }
-    SECTION( "Largest Algebraic" )
+    SECTION( "Largest Real" )
     {
-        run_test(mat, k, "la");
+        run_test(mat, k, "lr");
     }
-    SECTION( "Smallest Algebraic" )
+    SECTION( "Smallest Real" )
     {
-        run_test(mat, k, "sa");
+        run_test(mat, k, "sr");
+    }
+    SECTION( "Largest Imaginary" )
+    {
+        run_test(mat, k, "li");
+    }
+    SECTION( "Smallest Imaginary" )
+    {
+        run_test(mat, k, "si");
     }
 }
 
-TEST_CASE("Eigensolver of sparse symmetric real matrix [10x10]", "[eigs_sym]")
+TEST_CASE("Eigensolver of sparse general real matrix [10x10]", "[eigs_gen]")
 {
   arma::arma_rng::set_seed(123);
 
   arma::sp_mat A = arma::sprandu(10, 10, 0.3);
-  arma::sp_mat mat = A + A.t();
   uword k = 3;
 
-  run_test_sets(mat, k);
+  run_test_sets(A, k);
 }
 
-TEST_CASE("Eigensolver of sparse symmetric real matrix [100x100]", "[eigs_sym]")
+TEST_CASE("Eigensolver of sparse general real matrix [100x100]", "[eigs_gen]")
 {
     arma::arma_rng::set_seed(123);
 
     arma::sp_mat A = arma::sprandu(100, 100, 0.3);
-    arma::sp_mat mat = A + A.t();
     uword k = 10;
 
-    run_test_sets(mat, k);
+    run_test_sets(A, k);
 }
 
-TEST_CASE("Eigensolver of sparse symmetric real matrix [1000x1000]", "[eigs_sym]")
+TEST_CASE("Eigensolver of sparse general real matrix [1000x1000]", "[eigs_gen]")
 {
     arma::arma_rng::set_seed(123);
 
     arma::sp_mat A = arma::sprandu(1000, 1000, 0.3);
-    arma::sp_mat mat = A + A.t();
     uword k = 20;
 
-    run_test_sets(mat, k);
+    run_test_sets(A, k);
 }
