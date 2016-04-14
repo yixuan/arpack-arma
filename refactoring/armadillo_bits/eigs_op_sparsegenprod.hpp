83d1a267cf04eef0675ab27c122c83db2795b9c2
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-14 15:51:17 -0400

    avoid naming conflict

--- refactoring/armadillo_bits/eigs_op_sparsegenprod.hpp
+++ refactoring/armadillo_bits/eigs_op_sparsegenprod.hpp
@@ -18,29 +18,29 @@ class SparseGenMatProd
   {
   private:
 
-  const SpMat<eT>* mat;
+  const SpMat<eT>* op_mat;
 
 
   public:
 
   //! Constructor to create the matrix operation object.
   inline SparseGenMatProd(const SpMat<eT>& mat_)
-    : mat(&mat_)
+    : op_mat(&mat_)
   {}
 
   //! Return the number of rows of the underlying matrix.
-  arma_inline uword rows() const { return mat->n_rows; }
+  arma_inline uword rows() const { return op_mat->n_rows; }
 
   //! Return the number of columns of the underlying matrix.
-  arma_inline uword cols() const { return mat->n_cols; }
+  arma_inline uword cols() const { return op_mat->n_cols; }
 
   //! Perform the matrix-vector multiplication operation \f$y=Ax\f$.
   // y_out = A * x_in
   arma_inline void perform_op(eT* x_in, eT* y_out) const
     {
-    Col<eT> x(x_in , mat->n_cols, false);
-    Col<eT> y(y_out, mat->n_rows, false);
-    y = (*mat) * x;
+    Col<eT> x(x_in , op_mat->n_cols, false);
+    Col<eT> y(y_out, op_mat->n_rows, false);
+    y = (*op_mat) * x;
     }
   };
 
